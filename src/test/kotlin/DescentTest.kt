f6b60053c14e444afd6b0e3bd42546f570b0d571
Torsten Wiederkehr <torsten.wiederkehr@ing.com>
2023-08-29 08:08:11 +0200

    Use JUnit5 with AssertJ

---
+++ src/test/kotlin/DescentTest.kt
@@ -0,0 +1,15 @@
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class DescentTest {
+
+    @Test
+    fun should_run_the_test_only() {
+        // arrange
+
+        // act
+
+        // assert
+        Assertions.assertThat(true).isTrue()
+    }
+}
