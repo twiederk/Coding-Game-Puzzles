c09a95c3324aefdd4f893a5d9bf99f9889fb0ca3
Torsten Wiederkehr <torsten.wiederkehr@ing.com>
2024-04-02 15:27:20 +0200

    Implemented build tree

--- src/main/kotlin/BinarySearchTreeTraversal.kt
+++ src/main/kotlin/BinarySearchTreeTraversal.kt
@@ -22,10 +22,17 @@ fun main() {
 
 class BinarySearchTreeTraversal {
     fun buildTree(inputNumbers: List<Int>): Tree {
-        return Tree()
+        val tree = Tree(Node(inputNumbers[0]))
+        for (number in inputNumbers.subList(1, inputNumbers.size)) {
+            tree.add(Node(number))
+        }
+        return tree
     }
 
-    class Tree {
+    class Tree(
+        val root: Node
+    ) {
+
         fun preOrder(): String {
             return ""
         }
@@ -41,13 +48,34 @@ class BinarySearchTreeTraversal {
         fun levelOrder(): String {
             return ""
         }
+
+        fun add(node: Node) {
+            var current = root
+            while (true) {
+                if (node.key < current.key) {
+                    if (current.left == null) {
+                        current.left = node
+                        return
+                    } else {
+                        current = current.left!!
+                    }
+                } else {
+                    if (current.right == null) {
+                        current.right = node
+                        return
+                    } else {
+                        current = current.right!!
+                    }
+                }
+            }
+        }
     }
 
-    class Node(
-        var key: Int,
-        var left: Node? = null,
-        var right: Node? = null
+    data class Node(
+        val key: Int
     ) {
+        var left: Node? = null
+        var right: Node? = null
     }
 
 }
--- src/test/kotlin/BinarySearchTreeTraversalTest.kt
+++ src/test/kotlin/BinarySearchTreeTraversalTest.kt
@@ -1,7 +1,10 @@
+import BinarySearchTreeTraversal.Node
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class BinarySearchTreeTraversalTest {
+
+    private val test1 = listOf(8, 6, 13, 10, 5)
 
     @Test
     fun should_display_pre_order_for_test1() {
@@ -13,5 +16,54 @@ class BinarySearchTreeTraversalTest {
 
         // assert
         assertThat(result).isEqualTo("8 6 5 13 10")
+    }
+
+    @Test
+    fun should_build_tree_with_root_node() {
+
+        // act
+        val tree = BinarySearchTreeTraversal().buildTree(listOf(8))
+
+        // assert
+        assertThat(tree.root).isEqualTo(Node(8))
+    }
+
+    @Test
+    fun should_build_tree_with_root_and_left_child() {
+
+        // act
+        val tree = BinarySearchTreeTraversal().buildTree(listOf(8, 6))
+
+        // assert
+        assertThat(tree.root).isEqualTo(Node(8))
+        assertThat(tree.root.left).isEqualTo(Node(6))
+        assertThat(tree.root.right).isNull()
+    }
+
+    @Test
+    fun should_build_tree_with_root_and_right_child() {
+        // arrange
+
+        // act
+        val tree = BinarySearchTreeTraversal().buildTree(listOf(8, 10))
+
+        // assert
+        assertThat(tree.root).isEqualTo(Node(8))
+        assertThat(tree.root.right).isEqualTo(Node(10))
+        assertThat(tree.root.left).isNull()
+    }
+
+    @Test
+    fun should_built_tree_of_test_1() {
+
+        // act
+        val tree = BinarySearchTreeTraversal().buildTree(test1)
+
+        // assert
+        assertThat(tree.root).isEqualTo(Node(8))
+        assertThat(tree.root.left).isEqualTo(Node(6))
+        assertThat(tree.root.left?.left).isEqualTo(Node(5))
+        assertThat(tree.root.right).isEqualTo(Node(13))
+        assertThat(tree.root.right?.left).isEqualTo(Node(10))
     }
 }
