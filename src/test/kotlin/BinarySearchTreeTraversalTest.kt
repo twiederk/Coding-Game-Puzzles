28fd1933150fdbb1b5e1c24a7862a3dbf82531c3
Torsten Wiederkehr <torsten.wiederkehr@ing.com>
2024-04-02 13:28:31 +0200

    Created first test

--- src/test/kotlin/BinarySearchTreeTraversalTest.kt
+++ src/test/kotlin/BinarySearchTreeTraversalTest.kt
@@ -1,2 +1,17 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
 class BinarySearchTreeTraversalTest {
+
+    @Test
+    fun should_display_pre_order_for_test1() {
+        // arrange
+        val tree = BinarySearchTreeTraversal().buildTree(listOf(8, 6, 13, 10, 5))
+
+        // act
+        val result = tree.preOrder()
+
+        // assert
+        assertThat(result).isEqualTo("8 6 5 13 10")
+    }
 }
