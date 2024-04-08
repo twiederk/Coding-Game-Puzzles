a2bc95226cb6d15e18780079cc12d184a5871233
Torsten Wiederkehr <torsten.wiederkehr@ing.com>
2024-04-08 16:10:21 +0200

    Implemented floodfill

--- src/main/kotlin/MovesInMaze.kt
+++ src/main/kotlin/MovesInMaze.kt
@@ -21,10 +21,69 @@ fun main() {
 class MovesInMaze {
 
     fun solve(maze: List<String>): String {
-        // find start
-        // flood fill
+        val start = start(maze)
+        floodfill(maze, start)
         // create filled maze
         return maze.joinToString("\n")
     }
+
+    fun start(maze: List<String>): Point2D {
+        for (y in maze.indices) {
+            for (x in maze[y].indices) {
+                if (maze[y][x] == 'S') {
+                    return Point2D(x, y)
+                }
+            }
+        }
+        throw IllegalStateException("No start found")
+    }
+
+    fun floodfill(maze: List<String>, start: Point2D): Set<Flood> {
+        val queue = LinkedList<Flood>()
+        val seen = mutableSetOf<Flood>()
+
+        queue.add(Flood(start, 0))
+        while (!queue.isEmpty()) {
+
+            val curr = queue.poll()
+            if (curr.coords in seen.map { it.coords }) {
+                continue
+            }
+
+            seen.add(curr)
+            curr.coords.neighbors(maze).forEach { neighbor ->
+                queue.add(Flood(neighbor, curr.steps + 1))
+            }
+        }
+
+        return seen
+    }
+
+    data class Point2D(
+        val x: Int,
+        val y: Int
+    ) {
+        fun neighbors(maze: List<String>): List<Point2D> {
+            val neighbors = mutableListOf<Point2D>()
+            if (maze[y][x - 1] == '.') {
+                neighbors.add(Point2D(x - 1, y))
+            }
+            if (maze[y][x + 1] == '.') {
+                neighbors.add(Point2D(x + 1, y))
+            }
+            if (maze[y - 1][x] == '.') {
+                neighbors.add(Point2D(x, y - 1))
+            }
+            if (maze[y + 1][x] == '.') {
+                neighbors.add(Point2D(x, y + 1))
+            }
+            return neighbors
+        }
+    }
+
+    data class Flood(
+        val coords: Point2D,
+        val steps: Int
+    )
 
 }
--- src/test/kotlin/MovesInMazeTest.kt
+++ src/test/kotlin/MovesInMazeTest.kt
@@ -1,4 +1,6 @@
-import org.assertj.core.api.Assertions
+import MovesInMaze.Flood
+import MovesInMaze.Point2D
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class MovesInMazeTest {
@@ -18,7 +20,7 @@ class MovesInMazeTest {
         val result = MovesInMaze().solve(test1)
 
         // assert
-        Assertions.assertThat(result).isEqualTo(
+        assertThat(result).isEqualTo(
             """
             ##########
             #01234567#
@@ -27,6 +29,60 @@ class MovesInMazeTest {
             ##########
         """.trimIndent()
         )
-
     }
+
+    @Test
+    fun should_find_start_position() {
+
+        // act
+        val start = MovesInMaze().start(test1)
+
+        // assert
+        assertThat(start).isEqualTo(Point2D(1, 1))
+    }
+
+
+    @Test
+    fun should_flood_fill_maze() {
+
+        // act
+        val flood = MovesInMaze().floodfill(test1, Point2D(1, 1))
+
+        // assert
+//        ##########
+//        #01234567#
+//        ##2#####8#
+//        ##3#DCBA9#
+//        ##########
+        assertThat(flood).containsExactly(
+            Flood(Point2D(1, 1), 0),
+            Flood(Point2D(2, 1), 1),
+            Flood(Point2D(3, 1), 2),
+            Flood(Point2D(4, 1), 3),
+            Flood(Point2D(5, 1), 4),
+            Flood(Point2D(6, 1), 5),
+            Flood(Point2D(7, 1), 6),
+//            Flood(Point2D(1, 2), 2),
+//            Flood(Point2D(1, 3), 3),
+        )
+    }
+
+    //    "##########",
+//    "#S.......#",
+//    "##.#####.#",
+//    "##.#.....#",
+//    "##########"
+    @Test
+    fun should_return_neighbors_of_pos_1_1() {
+        // arrange
+
+        // act
+        val neighbors = Point2D(1, 1).neighbors(test1)
+
+        // assert
+        assertThat(neighbors).containsExactly(
+            Point2D(2, 1)
+        )
+    }
+
 }
