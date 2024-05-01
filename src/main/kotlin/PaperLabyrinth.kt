6d475e2b808cbe8eeca34cc268f5831f37670bd6
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2024-05-01 06:43:24 +0200

    Start implementing of Point2D.neighbors

--- src/main/kotlin/PaperLabyrinth.kt
+++ src/main/kotlin/PaperLabyrinth.kt
@@ -10,7 +10,7 @@ fun main() {
     val ys = input.nextInt()
     val xr = input.nextInt()
     val yr = input.nextInt()
-    val w = input.nextInt()
+    input.nextInt()
     val h = input.nextInt()
     val labyrinth = mutableListOf<String>()
     for (i in 0 until h) {
@@ -37,12 +37,51 @@ class PaperLabyrinth(
         System.err.println("start: $start")
         System.err.println("rabbit: $rabbit")
         System.err.println("labyrinth: $labyrinth")
+
+
+
         return Pair(0, 0)
     }
 
     data class Point2D(
         val x: Int,
         val y: Int
-    )
+    ) {
+        private val neighbors = mapOf(
+            '0' to listOf(LEFT, RIGHT, DOWN, TOP),
+            '1' to listOf(LEFT, RIGHT, TOP),
+            '2' to listOf(RIGHT, DOWN, TOP),
+            '3' to listOf(RIGHT, TOP),
+            '4' to listOf(LEFT, RIGHT, DOWN),
+            '5' to listOf(LEFT, RIGHT, DOWN, TOP),
+            '6' to listOf(LEFT, RIGHT, DOWN, TOP),
+            '7' to listOf(LEFT, RIGHT, DOWN, TOP),
+            '8' to listOf(LEFT, RIGHT, DOWN, TOP),
+            '9' to listOf(LEFT, RIGHT, DOWN, TOP),
+            'a' to listOf(LEFT, RIGHT, DOWN, TOP),
+            'b' to listOf(LEFT, RIGHT, DOWN, TOP),
+            'c' to listOf(LEFT, RIGHT, DOWN, TOP),
+            'd' to listOf(LEFT, RIGHT, DOWN, TOP),
+            'e' to listOf(LEFT, RIGHT, DOWN, TOP),
+            'f' to listOf(LEFT, RIGHT, DOWN, TOP),
+        )
+
+        fun next(cell: Char): List<Point2D> {
+            return neighbors[cell]!!.map { this + it }
+        }
+
+        operator fun minus(other: Point2D): Point2D =
+            Point2D(x - other.x, y - other.y)
+
+        operator fun plus(other: Point2D): Point2D =
+            Point2D(x + other.x, y + other.y)
+
+        companion object {
+            val TOP = Point2D(0, -1)
+            val DOWN = Point2D(0, 1)
+            val LEFT = Point2D(-1, 0)
+            val RIGHT = Point2D(1, 0)
+        }
+    }
 
 }
--- src/test/kotlin/PaperLabyrinthTest.kt
+++ src/test/kotlin/PaperLabyrinthTest.kt
@@ -1,2 +1,77 @@
+import PaperLabyrinth.Point2D
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
 class PaperLabyrinthTest {
+
+    @Test
+    fun should_find_neighbors_of_0() {
+
+        // act
+        val neighbors = Point2D(1, 1).next('0')
+
+        // assert
+        assertThat(neighbors).containsExactlyInAnyOrder(
+            Point2D(0, 1),
+            Point2D(1, 0),
+            Point2D(1, 2),
+            Point2D(2, 1)
+        )
+    }
+
+    @Test
+    fun should_find_neighbors_of_1() {
+
+        // act
+        val neighbors = Point2D(1, 1).next('1')
+
+        // assert
+        assertThat(neighbors).containsExactlyInAnyOrder(
+            Point2D(0, 1),
+            Point2D(1, 0),
+            Point2D(2, 1)
+        )
+    }
+
+    @Test
+    fun should_find_neighbors_of_2() {
+
+        // act
+        val neighbors = Point2D(1, 1).next('2')
+
+        // assert
+        assertThat(neighbors).containsExactlyInAnyOrder(
+            Point2D(1, 0),
+            Point2D(1, 2),
+            Point2D(2, 1)
+        )
+    }
+
+    @Test
+    fun should_find_neighbors_of_3() {
+
+        // act
+        val neighbors = Point2D(1, 1).next('3')
+
+        // assert
+        assertThat(neighbors).containsExactlyInAnyOrder(
+            Point2D(1, 0),
+            Point2D(2, 1)
+        )
+    }
+
+    @Test
+    fun should_find_neighbors_of_4() {
+
+        // act
+        val neighbors = Point2D(1, 1).next('4')
+
+        // assert
+        assertThat(neighbors).containsExactlyInAnyOrder(
+            Point2D(0, 1),
+            Point2D(1, 2),
+            Point2D(2, 1)
+        )
+    }
+
 }
